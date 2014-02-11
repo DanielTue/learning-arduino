f53b61b20b907cbc9c53940d4958a101346992ee
DanielTue <tuetravel@gmail.com>
2014-02-11 23:29:32 +0100

    Create reliable-push-button-to-control-LED.ino
    Turn on LED when the button is pressed and keep it on after it is released.

---
+++ reliable-push-button-to-control-LED.ino
@@ -0,0 +1,31 @@
+const int LED =13;
+const int BUTTON = 7;
+
+int state = 0;
+int val = 0;
+int old_val = 0;
+
+
+void setup() 
+{
+   pinMode(LED, OUTPUT); 
+   pinMode(BUTTON, INPUT);
+}
+
+void loop() 
+{
+ val = digitalRead(BUTTON);
+ 
+ if((val == HIGH) && (old_val == LOW)) {
+   state = 1 - state; 
+ }
+ 
+ old_val = val;
+ 
+ if(state == 1) {
+   digitalWrite(LED, HIGH);
+   delay(10); 
+ }else {
+    digitalWrite(LED, LOW);
+   delay(10); 
+ }
